705530ea77d7f526027dfe4ddb2834fef761d1e7
leegandhi18 <leegandhi18@gmail.com>
2020-12-04 18:21:45 +0900

    파일 생성 및 리다이렉션

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+var qs = require('querystring');
 var url = require('url');
 
 function templateHTML(title, list, body) {
@@ -7,16 +8,28 @@ function templateHTML(title, list, body) {
                 <!doctype html>
                 <html>
                 <head>
-                <title>WEB1 - ${title}</title>
+                <title>WEB - ${title}</title>
                 <meta charset="utf-8">
                 </head>
                 <body>
                 <h1><a href="/">WEB</a></h1>
                 ${list}
+                <a href="/create">create</a>
                 ${body}
                 </body>
                 </html>
                 `;
+}
+
+function templateList(filelist) {
+    var list = '<ul>';
+    var i = 0;
+    while (i < filelist.length) {
+        list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+        i = i + 1;
+    }
+    list = list + '</ul>';
+    return list;
 }
 
 var app = http.createServer(function (request, response) {
@@ -31,21 +44,7 @@ var app = http.createServer(function (request, response) {
             fs.readdir('./data', function (err, filelist) {
                 var title = 'Welcome';
                 var description = 'Hello, Node.js';
-
-                // var list = `<ul>
-                //     <li><a href="/?id=HTML">HTML</a></li>
-                //     <li><a href="/?id=CSS">CSS</a></li>
-                //     <li><a href="/?id=JavaScript">JavaScript</a></li>
-                // </ul>`
-
-                // 주석처리한 코드를 밑에 처럼 표현
-                var list = '<ul>';
-                var i = 0;
-                while (i < filelist.length) {
-                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-                    i = i + 1;
-                }
-                list = list + '</ul>';
+                var list = templateList(filelist);
                 var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
                 response.writeHead(200);
                 response.end(template);
@@ -54,28 +53,48 @@ var app = http.createServer(function (request, response) {
 
         } else {
             fs.readdir('./data', function (err, filelist) {
-                // var list = `<ul>
-                //     <li><a href="/?id=HTML">HTML</a></li>
-                //     <li><a href="/?id=CSS">CSS</a></li>
-                //     <li><a href="/?id=JavaScript">JavaScript</a></li>
-                // </ul>`
-
-                // 주석처리한 코드를 밑에 처럼 표현
-                var list = '<ul>';
-                var i = 0;
-                while (i < filelist.length) {
-                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-                    i = i + 1;
-                }
-                list = list + '</ul>';
+                
                 fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
                     var title = queryData.id;
+                    var list = templateList(filelist);
                     var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
                     response.writeHead(200);
                     response.end(template);
                 });
             });
         }
+    } else if(pathname === '/create') {
+        fs.readdir('./data', function (err, filelist) {
+            var title = 'WEB - create';
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `
+            <form action="http://localhost:3000/create_process" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p>
+                    <textarea name="description" placeholder="description"></textarea>
+                </p>
+                <p>
+                    <input type="submit">
+                </p>
+            <form>
+            `);
+            response.writeHead(200);
+            response.end(template);
+        });
+    } else if(pathname === '/create_process') {
+        var body = '';
+        request.on('data', function(data) {
+            body += data;
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            var title = post.title;
+            var description = post.description;
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+                response.writeHead(302, {Location:`/?id=${title}`});
+                response.end('success');
+            })
+        });
     } else {
         response.writeHead(404);
         response.end('Not found');
